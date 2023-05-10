eeee97275374fc57ea42c6666a71aa42e3165135
uerbzr <nigel.sibbert@gmail.com>
2023-05-10 09:03:04 +0100

    Add project files.

---
+++ csharp-singleclass-bootcamp.Tests/BootCampAcademyTests.cs
@@ -0,0 +1,24 @@
+﻿using csharp_singleclass_bootcamp.main;
+using NUnit.Framework;
+
+namespace csharp_singleclass_bootcamp.Tests
+{
+    [TestFixture]
+    public class BootCampAcademyTests
+    {
+        [TestCase("C# for experts")]
+        public void AddCourseToLearningPlan(string course)
+        {
+            //arrange
+            BootCampAcademy academy = new BootCampAcademy();
+
+            //act
+            academy.AddToLearningPlan(course);
+
+            //assert
+            Assert.AreEqual(academy.LearningPlans.Count, 1);
+        }
+
+
+    }
+}
---
+++ csharp-singleclass-bootcamp.main/BootCampAcademy.cs
@@ -0,0 +1,34 @@
+﻿namespace csharp_singleclass_bootcamp.main
+{
+
+    /*
+
+Bootcamp Academy
+
+
+
+1. As a student of the Academy I want to be able to add a specific course to my Learning Plan.
+
+
+2. As a student of the Academy if I change my mind I want to be able to remove a course from my Learning Plan.
+
+3. As a student of the Academy I'd like to know when my Learning plan is full, when I try to add another item (Course, Tutorial, Certification) to it.
+
+4. As a manager at the Academy, I'd like to be able to change the capacity of Learning Plans.
+
+
+5. As a student of the Academy, I'd like to know that if I try to remove an item that doesn't exist in my Learning Plan.
+     */
+    public class BootCampAcademy
+    {
+
+        public void AddToLearningPlan(string course)
+        {
+            throw new NotImplementedException();
+        }
+        
+        
+        public List<string> LearningPlans { get; set; }
+
+    }
+}
